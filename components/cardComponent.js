8b47e37f03552f6334bb742bf59867af5b577b86
vanndeth <vanndeth@gmail.com>
2024-02-21 00:06:00 +0700

    modified card

--- components/cardComponent.js
+++ components/cardComponent.js
@@ -1,6 +1,6 @@
 export const cardComponent = (product) => {
     return `<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
+    <a href="#" class="block w-[250px] h-[250px] overflow-hidden">
         <img class="p-8 rounded-t-lg" src="${product.image}" alt="product image" />
     </a>
     <div class="px-5 pb-5">
--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict'
-import { cardComponent } from "../components/cardComponent";
+import { cardComponent } from "../components/cardComponent.js";
 
 let renderArea = document.querySelector('#render-area');
 console.count(renderArea);
 
-import { products } from "../data/products";
+import { products } from "../data/products.js";
 
 products.map((product) => {
     renderArea.innerHTML += cardComponent(product);
